242d4a48f38654daf461f8df0bf84f2b7458d4af
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-10-18 23:07:35 +0530

    Updates

---
+++ precache-manifest.64406ee350bfb2ea08a2272516dec599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcafca873f407ba129b220d9e019003",
+    "url": "/twitterClone/index.html"
+  },
+  {
+    "revision": "6048ec3beae836d38b51",
+    "url": "/twitterClone/static/js/2.8951410f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/twitterClone/static/js/2.8951410f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7399cae720497aa486",
+    "url": "/twitterClone/static/js/main.43c4c7fc.chunk.js"
+  },
+  {
+    "revision": "ebe4c0cb234477415d6b",
+    "url": "/twitterClone/static/js/runtime-main.d5b0c6be.js"
+  }
+]);
